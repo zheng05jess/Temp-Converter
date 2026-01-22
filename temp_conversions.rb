2f6f6939399036b0bb2764e3583320c1a716f09a
Jessica Zheng <jzheng5@andrew.cmu.edu>
2026-01-22 13:45:18 -0500

    Added non-integer tests to file

--- temp_conversions.rb
+++ temp_conversions.rb
@@ -1,4 +1,5 @@
 def convert(temp)
+  return "Temperature must be an integer" unless temp.class == Integer
   5 * (temp - 32)/9
 end
 
@@ -7,3 +8,4 @@ puts "Converting Fahrenheit to Celsius"
 puts convert(32)
 puts convert(50)
 puts convert(212)
+puts convert("zero")
